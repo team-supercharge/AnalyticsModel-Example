e845ca3f2c16e9ee34b0de6c1ac8f01519ea41ed
Csaba Szugyiczki <csaba.szugyiczki@supercharge.io>
2020-08-04 15:53:20 +0200

    Add basic databinding with logic

--- app/src/main/java/io/supercharge/hiltexample/main/model/MainMenuViewModel.kt
+++ app/src/main/java/io/supercharge/hiltexample/main/model/MainMenuViewModel.kt
@@ -1,10 +1,23 @@
 package io.supercharge.hiltexample.main.model
 
+import android.view.View
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainMenuViewModel @ViewModelInject internal constructor() : ViewModel() {
 
-    val textValue = MutableLiveData("Hilt")
+    private val _value = MutableLiveData(0)
+
+    val value: LiveData<Int>
+        get() = _value
+
+    val incrementValue = View.OnClickListener {
+        _value.postValue(_value.value!! + 1)
+    }
+
+    val decrementValue = View.OnClickListener {
+        _value.postValue(_value.value!! - 1)
+    }
 }
--- app/src/main/java/io/supercharge/hiltexample/main/ui/MainMenuFragment.kt
+++ app/src/main/java/io/supercharge/hiltexample/main/ui/MainMenuFragment.kt
@@ -1,66 +1,46 @@
 package io.supercharge.hiltexample.main.ui
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.app.NotificationCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import io.supercharge.hiltexample.R
+import io.supercharge.hiltexample.BR
 import io.supercharge.hiltexample.main.model.MainMenuViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainMenuFragment : Fragment() {
 
     private val viewModel: MainMenuViewModel by viewModels()
 
-    @Inject
-    lateinit var notificationManager: NotificationManager
+    private var binding: ViewDataBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
-
-        val textView = view.findViewById<TextView>(R.id.text)
-
-        viewModel.textValue.asFlow()
-            .onEach { text ->
-                textView.text = text
-                createNotification(text)
-            }
-            .launchIn(viewLifecycleOwner.lifecycleScope)
-
-        return view
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_main,
+            container,
+            false
+        )
+        return binding?.root
     }
 
-    private fun createNotification(text: String) {
-        val channelId = "hilt"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val builder = NotificationCompat.Builder(requireContext(), channelId)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle(text)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel =
-                NotificationChannel(channelId, text, NotificationManager.IMPORTANCE_DEFAULT)
-            notificationManager.createNotificationChannel(channel)
+        binding?.run {
+            lifecycleOwner = this@MainMenuFragment
+            setVariable(BR.vm, viewModel)
+            executePendingBindings()
         }
-
-        notificationManager.notify(text.hashCode(), builder.build())
     }
 }
